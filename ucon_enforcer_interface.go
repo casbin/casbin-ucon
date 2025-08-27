91afe7b3a463afa82cf37a47d8e489eb5cb55f90
Zhendong Liu <148350506+bugoutianzhen123@users.noreply.github.com>
2025-08-27 21:03:21 +0800

    feat: add initial code

---
+++ ucon_enforcer_interface.go
@@ -0,0 +1,45 @@
+// Copyright 2025 The casbin Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ucon
+
+import (
+	"github.com/casbin/casbin/v2"
+)
+
+// IUconEnforcer is the API interface of UconEnforcer.
+type IUconEnforcer interface {
+	// Inherit Casbin basic functionality
+	casbin.IEnforcer
+
+	// Enhanced enforcement with session context
+	EnforceWithSession(sessionID string) (bool, error)
+
+	// Session management
+	CreateSession(sub string, act string, obj string, attributes map[string]interface{}) (string, error)
+	GetSession(sessionID string) (*Session, error)
+	RevokeSession(sessionID string) error
+
+	// Condition evaluation
+	AddCondition(condition *Condition) error
+	EvaluateConditions(sessionID string) (bool, error)
+
+	// Obligation management
+	AddObligation(obligation *Obligation) error
+	ExecuteObligations(sessionID string) error
+
+	// Continuous monitoring
+	StartMonitoring(sessionID string) error
+	StopMonitoring(sessionID string) error
+}

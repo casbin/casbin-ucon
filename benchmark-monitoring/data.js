a335c40afff8e798b2b8aed63efbcf62ea348711
github-action-benchmark <github@users.noreply.github.com>
2025-09-03 12:42:54 +0000

    add Benchmark (go) benchmark result for 2c7d1ba3c398638790d3b1dc6f0cb5327bcf9082

---
+++ benchmark-monitoring/data.js
@@ -0,0 +1,56 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1756903374102,
+  "repoUrl": "https://github.com/casbin/casbin-ucon",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "148350506+bugoutianzhen123@users.noreply.github.com",
+            "name": "Zhendong Liu",
+            "username": "bugoutianzhen123"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2c7d1ba3c398638790d3b1dc6f0cb5327bcf9082",
+          "message": "feat: fix semantic-release CI (#5)",
+          "timestamp": "2025-09-03T16:02:28+08:00",
+          "tree_id": "3b8c154ffd67d2310636bce90377d12c33082334",
+          "url": "https://github.com/casbin/casbin-ucon/commit/2c7d1ba3c398638790d3b1dc6f0cb5327bcf9082"
+        },
+        "date": 1756903373167,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkPlaceholder",
+            "value": 0.3112,
+            "unit": "ns/op\t       0 B/op\t       0 allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPlaceholder - ns/op",
+            "value": 0.3112,
+            "unit": "ns/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPlaceholder - B/op",
+            "value": 0,
+            "unit": "B/op",
+            "extra": "1000000000 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkPlaceholder - allocs/op",
+            "value": 0,
+            "unit": "allocs/op",
+            "extra": "1000000000 times\n4 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
